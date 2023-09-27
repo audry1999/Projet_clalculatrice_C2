58f856b5794f1a46e05b17090a6a1e2286973dd8
audry1999 <audrykaj@gamil.com>
2023-09-27 16:48:51 +0200

    mon calculatrice

---
+++ index.js
@@ -0,0 +1,14 @@
+//  calculatrice
+
+function calculatrice(number1, number2, operation){
+    return operation(number1, number2)
+}
+
+ function add( nbr1, nbr2){
+    return nbr1 + nbr2;
+
+ }
+ function substract(br1, nbr2){
+    return nbr2 - nbr2;
+
+ }
